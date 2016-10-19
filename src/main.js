b5ee269f6bd358034847e75b858d8dc297bb0134
Michael Mendoza <m.mendoza711@gmail.com>
2016-10-19 15:52:28 -0600

    Incremented version number

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@
 import CSS from "./styles/main.scss";
 import AppView from './components/views/app-view.jsx';
 
-console.log('Image Viewer -','Version: 0.0.8', 'Date:Oct 10, 2016');
+console.log('Image Viewer -','Version: 0.0.9', 'Date:Oct 19, 2016');
