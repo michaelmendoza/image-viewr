29f275d8dfa995c1210edf5020eb0a43749213fa
Michael Mendoza <m.mendoza711@gmail.com>
2016-10-26 10:27:54 -0600

    Update to server. Added build folder temporarily

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 
 // set up access to index.html
-app.use(express.static(path.join(__dirname, '../src')));
+app.use(express.static(path.join(__dirname, '../build')));
 app.get('/', function(req, res) {
 	console.log('Getting index.html');
-  res.sendFile(path.join(__dirname, '../src', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 // set up the RESTful API, handler methods are defined in api.js
