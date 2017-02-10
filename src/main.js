ba943e58eccaacccace9350fbd93e014dcfc38ca
Michael Mendoza <m.mendoza711@gmail.com>
2017-02-09 17:50:16 -0700

    set version to 0.0.33

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@
 import CSS from "./styles/main.scss";
 import AppView from './components/views/app-view.jsx';
 
-console.log('Image Viewer -','Version: 0.0.32', 'Date:Jan 4th, 2016');
+console.log('Image Viewer -','Version: 0.0.33', 'Date:Feb 9th, 2016');
