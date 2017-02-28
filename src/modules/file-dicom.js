9a9b7bf08af1e076a46802c1fa764501729f58b4
Michael Mendoza <m.mendoza711@gmail.com>
2017-02-28 12:25:14 -0700

    Updated file-dicom to retrieve more header information from dicoms

--- src/modules/file-dicom.js
+++ src/modules/file-dicom.js
@@ -11,15 +11,43 @@ class FileDICOM {
 		var frame = 0;
 		var arrayBuffer = event.target.result;
 		var byteArray = new Uint8Array(arrayBuffer);
-
 		var dataSet = dicomParser.parseDicom(byteArray);
-		var patientId = dataSet.string('x00100020');
-		var patientName = dataSet.string('x00100010');
-		var transferSyntax = dataSet.string('x00020010');  // Explicit VR Little Endian - "1.2.840.10008.1.2.1"
 
-		var rows = dataSet.uint16('x00280010');
-		var columns = dataSet.uint16('x00280011');
-		var samplesPerPixel = dataSet.uint16('x00280002');
+		var headerUIDs = {
+			patientId: 'x00100020',
+			patientName: 'x00100010',
+			studyId: 'x00200010',
+			studyDescription: 'x00081030',
+			studyDate: 'x00080020',
+			studyTime: 'x00080030',
+			protocolName: 'x00181030',
+			seriesDescription: 'x0008103e',
+			seriesNumber: 'x00200011',
+			modality: 'x00080060',
+			instanceNumber: 'x00200013',
+			acquistionNumber: 'x00200012',
+			
+			rows: 'x00280010',
+			columns: 'x00280011',
+			samplesPerPixel: 'x00280002',
+			pixelSpacing: 'x00280030',
+			sliceThickness: 'x00180050',
+			bitsStored: 'x00280101',
+
+			transferSyntax: 'x00020010' // Explicit VR Little Endian - "1.2.840.10008.1.2.1"
+		};
+
+		var header = {};
+		for (var key in headerUIDs) {
+			if(key == 'rows' || key == 'columns' || key == 'samplesPerPixel' || key == 'bitsStored')
+				header[key] = dataSet.uint16(headerUIDs[key]);
+			else
+				header[key] = dataSet.string(headerUIDs[key]);
+		}
+
+		var rows = header.rows;
+		var columns = header.columns;
+		var samplesPerPixel = header.samplesPerPixel;
 		var numPixels = rows * columns * samplesPerPixel;
 
 		var pixelDataElement = dataSet.elements.x7fe00010;
@@ -45,8 +73,6 @@ class FileDICOM {
 		for(var i = 0; i < numPixels; i++) {
 			maxValue = maxValue >= pixelData[i] ? maxValue : pixelData[i];
 		}
-
-		var bitsStored = dataSet.uint16('x00280101'); 
 		var resolution = maxValue; //Math.pow(2,bitsStored);
 
 		for(var i = 0; i < numPixels; i++) {
@@ -62,16 +88,12 @@ class FileDICOM {
 		var img = document.createElement('img');
 		img.src = dataURL;
 		
+		this.header = header;
 		this.img = img;
 		this.width = columns;
 		this.height = rows;
 		this.numPixels = numPixels;
 		this.pixelData = pixelData;
-	}
-
-	getDicomMetadata() {
-		var pixelspacing = dataSet.uint16('x00280030');
-		var slicethickness = dataSet.uint16('x00180050');
 	}
 
 	getPixelFormat(dataSet) {
