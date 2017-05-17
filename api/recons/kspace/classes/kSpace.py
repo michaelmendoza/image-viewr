700451773a12d926a01c3cb195c1e8fee02c4baa
mckib2 <nicholas.bgp@gmail.com>
2017-05-17 10:41:46 -0600

    Created basic recon for kspace

---
+++ api/recons/kspace/classes/kSpace.py
@@ -0,0 +1,48 @@
+import numpy
+import time
+
+class kSpace(object) :
+    # Getter and setter for image reconstruction validation:
+    def get_image(self) :
+        if self._image is None :
+            raise ValueError("Data must be reconstructed before an image is available!")
+        else :
+            return self._image
+
+    def set_image(self, image) :
+        self._image = image
+
+    # Member variables:
+    image = property(get_image, set_image)
+    
+    # Constructor:
+    def __init__(self, initialData) :
+        self.data = self.validateData(initialData)
+        self._image = None
+
+    # Methods:
+    def validateData(self, data) :
+        if type(data) is not numpy.ndarray :
+            raise TypeError("The constructor requires a numpy array!")
+        elif (len(data.shape) is not 2) and (len(data.shape) is not 3) :
+            raise ValueError("The array must be two or three dimensional!")
+        return data
+
+    # Recon Methods:
+    def IFT(self) :
+        if len(self.data.shape) is 2 :
+            tas = time.time()
+            self.image = numpy.fft.ifft2(self.data)
+            tas = time.time() - tas
+            print("2D IFFT computed in: ", tas)
+        else :
+            tas = time.time()
+            self.image = numpy.fft.ifftn(self.data)
+            tas = time.time() - tas
+            print("3D IFFT computed in: ", tas)
+            
+    def ellipticalRecon(self) :
+        print("Perform elliptical model recon")
+
+    def spiralRecon(self) :
+        print("Perform spiral recon")
---
+++ api/recons/kspace/tests/test_recon_2d_ifft.py
@@ -0,0 +1,87 @@
+import unittest
+import numpy
+import scipy
+
+import sys
+sys.path.insert(0, '../')
+from classes.kSpace import kSpace
+
+class Simple2DkSpaceTestCase(unittest.TestCase) :
+    def setUp(self) :
+        self.kspace = kSpace(numpy.array([ [1, 2, 3, 4, 5],
+                                           [1, 2, 3, 4, 5],
+                                           [1, 2, 3, 4, 5],
+                                           [1, 2, 3, 4, 5],
+                                           [1, 2, 3, 4, 5] ]))
+
+    def tearDown(self) :
+        self.kspace = None
+
+class Simple3DkSpaceTestCase(unittest.TestCase) :
+    def setUp(self) :
+        self.kspace = kSpace(numpy.array([
+            [ [1, 2, 3], [1, 2, 3], [1, 2, 3] ],
+            [ [1, 2, 3], [1, 2, 3], [1, 2, 3] ],
+            [ [1, 2, 3], [1, 2, 3], [1, 2, 3] ] ]))
+    def tearDown(self) :
+        self.kspace = None
+            
+
+        
+class Zeros2DkSpaceTestCase(unittest.TestCase) :
+    def setUp(self) :
+        self.kspace = kSpace(numpy.zeros((5, 5)))
+
+    def tearDown(self) :
+        self.kspace = None
+
+class Zeros3DkSpaceTestCase(unittest.TestCase) :
+    def setUp(self) :
+        self.kspace = kSpace(numpy.zeros((5, 5, 5)))
+    def tearDown(self) :
+        self.kspace = None
+        
+class Inverse2DFTReconstructionTestCase(Simple2DkSpaceTestCase) :
+    def test_image_is_not_accessible_before_recon(self) :
+        with self.assertRaises(ValueError) :
+            image = self.kspace.image
+
+    def test_result_matches_matlab(self) :
+        image = self.kspace.IFT()
+        matlab = numpy.array([
+            [3+0j, -0.5-0.688190960235587j, -0.5-0.162459848116453j, -0.5+0.162459848116453j, -0.5+0.688190960235587j],
+            [0+0j, 0+0j, 0+0j, 0+0j, 0+0j],
+            [0+0j, 0+0j, 0+0j, 0+0j, 0+0j],
+            [0+0j, 0+0j, 0+0j, 0+0j, 0+0j],
+            [0+0j, 0+0j, 0+0j, 0+0j, 0+0j]
+        ])
+
+        isCloseEnough = numpy.allclose(self.kspace.image, matlab)
+        self.assertTrue(isCloseEnough)
+        
+class ValidationTestCases(Zeros3DkSpaceTestCase) :
+    def test_require_numpy_array(self) :
+        self.assertEqual(type(self.kspace.data), numpy.ndarray)
+        with self.assertRaises(TypeError) :
+            kspace = kSpace([[1, 2], [1, 2]])
+
+    def test_require_two_or_three_dimensions(self) :
+        self.assertEqual(self.kspace.data.shape, (5, 5, 5))
+        with self.assertRaises(ValueError) :
+            kspace = kSpace(numpy.zeros((1)))
+            
+
+class Inverse3DFTReconstructionTestCase(Simple3DkSpaceTestCase) :
+    def test_image_is_not_accessible_before_recon(self) :
+        with self.assertRaises(ValueError) :
+            image = self.kspace.image
+
+    def test_result_matches_matlab(self) :
+        image = self.kspace.IFT()
+        matlab = numpy.zeros((3, 3, 3))
+        isCloseEnough = numpy.allclose(self.kspace.image, matlab)
+        self.assertTrue(isCloseEnough)
+
+            
+if __name__ == '__main__' :
+    unittest.main()
