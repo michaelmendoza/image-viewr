e4a234cb49aed9da1af495caf39db1a566872fb9
Michael Mendoza <m.mendoza711@gmail.com>
2017-06-30 15:43:05 -0600

    Updated version to 0.0.54

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@
 import CSS from "./styles/main.scss";
 import AppView from './components/views/app-view.jsx';
 
-console.log('Image Viewer -','Version: 0.0.53', 'Date:June 13, 2017');
+console.log('Image Viewer -','Version: 0.0.54', 'Date:June 30, 2017');
