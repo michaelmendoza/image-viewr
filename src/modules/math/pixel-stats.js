69dd602d830fc3bd78c197af0ca2fdf03ab4c22e
Michael Mendoza <m.mendoza711@gmail.com>
2017-10-13 11:35:05 -0600

    Fixed mean/std dev calculations for roi

--- src/modules/math/pixel-stats.js
+++ src/modules/math/pixel-stats.js
@@ -19,32 +19,42 @@ class PixelStats {
 		return { min:min, max:max };
 	}
 
+	/**
+	 * Calculate the mean of the pixel data with in the threshold bounds
+	 */
 	getMean(data, threshold) {
 		var sum = 0;
+		var count = 0;
 		var length = data.length;
 		for (var i = 0; i < length; i++) {
 			var value = data[i];
 			if(value >= 0 && value >= threshold.min && value <= threshold.max) {
 				sum += value;
+				count++
 			}
 		}
-		return sum / length;
+		return sum / count;
 	}
 
-	getStdDev(data, threshold) {	
+	/**
+	 * Calculate the mean of the pixel data with in the threshold bounds
+	 */
+	getStdDev(data, threshold) {
 		var sum = 0;
 		var sum2 = 0;
+		var count = 0;
 		var length =  data.length;
 		for (var i = 0; i < length; i++) {
 			var value = data[i];
 			if(value >= 0 && value >= threshold.min && value <= threshold.max) {
 				sum += value;
 				sum2 += value * value;
+				count++;
 			}
 		}
 
-		var mean = sum / length;
-		var mean2 = sum2 / length;
+		var mean = sum / count;
+		var mean2 = sum2 / count;
 
 		var variance = mean2 - mean * mean;
 		return Math.sqrt(variance);
