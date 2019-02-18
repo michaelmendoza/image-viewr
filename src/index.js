d2907ed278d8ed3821606a65b16395b20bd6665c
Michael Mendoza <m.mendoza711@gmail.com>
2019-02-17 23:04:29 -0700

    Updated npm packages. Now uses Webpack 4, Babel 7, React 16.8, etc.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@
 import CSS from "./styles/main.scss";
 import AppView from './components/views/app-view.jsx';
 
-console.log('Image Viewer -','Version: 0.0.56', 'Date:Nov 3, 2017');
+console.log('Image Viewer -','Version: 0.0.57', 'Date:Dec 17, 2019');
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,25 @@
 
-//module.exports = require('./webpack.lib.js');
-module.exports = require('./webpack.dev.js');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.(jpg|png)$/, loader: 'url?limit=25000' },
+      { test: /\.scss$/, loader: "style-loader!css-loader!sass-loader" }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+      path: path.resolve(__dirname, 'dist')
+    })
+  ]
+};
